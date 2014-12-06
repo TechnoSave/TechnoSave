c3a0b846ab379320f3787861b9be98ac72bfa262
Dave Mun <mun.dave@gmail.com>
2014-12-06 13:00:20 -0800

    Refactored backend third party API interaction.

--- js/parserFile.js
+++ js/parserFile.js
@@ -1,5 +1,17 @@
+var request = require('request');
+
+    //==============Walmart API URIs======================
+    var searchUrl = 'http://api.walmartlabs.com/v1/search';
+    var itemIDLookupUrl = 'http://api.walmartlabs.com/v1/items/';
+    var APIKey = '4z8pkk2ycuvewyydr4mf3ha5';
+    //==============Walmart API URIs======================
+
 module.exports = {
   parser : function(req, res){
-    res.send(req.body.items);
+    //var getData = (searchUrl + '?apiKey=' + APIKey + '&query=' + item);
+    request("http://api.walmartlabs.com/v1/search?apiKey=4z8pkk2ycuvewyydr4mf3ha5&query=tomato", function(error, response, body){
+      console.log(body);
+        res.send(body);
+    });
   }
-}
+};
