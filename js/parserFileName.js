dcd89a42778ecc51642871bcb81351e4fd34367a
Phil Elauria <phil.elauria@gmail.com>
2014-12-06 11:45:59 -0800

    added get request in parser function

--- js/parserFileName.js
+++ js/parserFileName.js
@@ -11,19 +11,15 @@ module.exports = {
 
     //https://docs.angularjs.org/api/ng/service/$http
     itemsList.forEach(function(item){
-      var testItem = "https://api.walmartlabs.com/v1/search?apiKey=4z8pkk2ycuvewyydr4mf3ha5&query=tomato";
+      var testItem = "http://api.walmartlabs.com/v1/search?apiKey=4z8pkk2ycuvewyydr4mf3ha5&query=tomato";
       var getData = (searchUrl + '?apiKey=' + APIKey + '&query=' + item);
      //http://api.walmartlabs.com/v1/search?apiKey=4z8pkk2ycuvewyydr4mf3ha5&query=potato
       $http.get(testItem)     
       .success(function(data){
         console.log('data from server', data);
-      })
-      .error(function(){
-        console.log('ERROR!!!!!!!!!!!!!!!');
       });
   })
 
 
   res.send(req.body.items);
-  }
 }
