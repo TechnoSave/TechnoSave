e545bdae1725de11e25a7a7fc35c630f83cded9a
Dave Mun <mun.dave@gmail.com>
2014-12-09 01:31:55 -0800

    Implemented BestBuy API crawling and updated Angular view to reflect different stores.

--- js/parserFile.js
+++ js/parserFile.js
@@ -6,20 +6,30 @@ var request = Promise.promisify(require('request'));
     var itemIDLookupUrl = 'http://api.walmartlabs.com/v1/items/';
     var APIKey = '4z8pkk2ycuvewyydr4mf3ha5';
     //==============Walmart API URIs======================
+
+    //==============BestBuy API URIs======================
+    var BBsearchUrl = 'http://api.remix.bestbuy.com/v1/products';
+    var BBAPIKey = '3fywvy298naxeed665ex82z5';
+    //==============BestBuy API URIs======================
  
 module.exports = {
   parser : function(req, res){
     //create itemsList array of user inputs, whitespace sanitized and put in array
     var itemsList = req.body.items.replace(/ /g,'').split(','),
         output = [],
-        max = 1,
-        completed = 0;
+        max = 1,        //number of matching requests per API per item
+        APIs = 2,       //number of APIs were using
+        completed = 0,  //number of completed async http req
+        queued = itemsList.length*APIs*max; //number of queued async http req
 
     //go through itemsList array of user inputs and get price for each from Wal-Mart API
     for(var i = 0; i < itemsList.length; i++){
         //construct GET url
         var getURL = (searchUrl + '?apiKey=' + APIKey + '&query=' + itemsList[i]);
-       
+        //http://api.remix.bestbuy.com/v1/products(name=cable*)?show=name,salePrice,sku&format=json&apiKey=3fywvy298naxeed665ex82z5
+        var BBgetURL = (BBsearchUrl + '(name=' + itemsList[i] + '*)?show=name,salePrice,sku&format=json' + '&apiKey=' + BBAPIKey);
+
+        //=================Walmart API requests====================
         //ask walmart API for data
         request(getURL)
         //async callback to parse data
@@ -37,6 +47,7 @@ module.exports = {
                 apiItem.price = itemObj.salePrice || itemObj.msrp;
                 apiItem.itemId = itemObj.itemId;
                 apiItem.name = itemObj.name;
+                apiItem.store = "WalMart";
                 
                 //store item info obj (that is a possible match) into the output
                 output.push(apiItem);
@@ -45,10 +56,44 @@ module.exports = {
 
             //if query that just finished is the last item in the search terms,
             //send data back to client
-            if(completed === itemsList.length){
+            if(completed === queued){
                 res.send(output);
             } 
         });
+        //=================Walmart API requests====================
+
+        //=================BestBuy API requests====================
+        //ask walmart API for data
+        request(BBgetURL)
+        //async callback to parse data
+        .then(function(data){
+            //create list of item objects - limited to var "max" results per item.
+            var apiItemsList = JSON.parse(data[0].body).products.slice(0, max)
+            
+            //for list of matching products, trim object and add to output  
+            apiItemsList.forEach(function(itemObj){
+                //construct new item object
+                var apiItem = {};
+
+                //get price, itemID, and name
+                    //if salePrice does not exist, default to MSRP
+                apiItem.price = itemObj.salePrice;
+                apiItem.itemId = itemObj.sku;
+                apiItem.name = itemObj.name;
+                apiItem.store = "BestBuy";
+                
+                //store item info obj (that is a possible match) into the output
+                output.push(apiItem);
+                completed++;
+            });
+
+            //if query that just finished is the last item in the search terms,
+            //send data back to client
+            if(completed === queued){
+                res.send(output);
+            } 
+        });
+        //=================BestBuy API requests====================
     }
   }
 }
