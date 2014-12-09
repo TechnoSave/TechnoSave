a25c1dd1659015ce366eb3f08fa1b9a3e8ca4729
Dave Mun <mun.dave@gmail.com>
2014-12-09 02:01:43 -0800

    Implemented cart click system for items.

--- public/lib/index.js
+++ public/lib/index.js
@@ -17,10 +17,27 @@ app.controller('ItemListCtrl', ['$scope', '$http', function ($scope, $http) {
   console.log('ItemListCtrl was loaded!');
 
   $scope.addItem = function() {
-    $http.post('/', {items: $scope.itemsModel})
+    $http.post('/', {items: $scope.inputModel})
       .success(function(data){
-        console.log(data);
-        $scope.items = data;
+        
+        $scope.items = $scope.items || [];
+
+        data.forEach(function(item){
+          $scope.items.push(item);
+        });
+        console.log($scope.items);
+
       });
   };
+
+  $scope.addToCart = function(element) {
+    $scope.cart = $scope.cart || [];
+    $scope.cart.push(element);
+    for(var i = 0; i < $scope.items.length; i++){
+      if($scope.items[i] === element){
+        $scope.items[i].splice(i, 1);
+      }
+    }
+  };
+
 }]);
