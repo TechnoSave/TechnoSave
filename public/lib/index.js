1e15551598d1b0db3c1fda307b0e0c16d4432124
Dave Mun <mun.dave@gmail.com>
2014-12-09 02:58:50 -0800

    Started implementation on shopping cart feature, UI elements(close button, tooltips).

--- public/lib/index.js
+++ public/lib/index.js
@@ -15,29 +15,37 @@ app.config(function($stateProvider, $urlRouterProvider) {
 app.controller('ItemListCtrl', ['$scope', '$http', function ($scope, $http) {
   
   console.log('ItemListCtrl was loaded!');
+  $scope.tip = false;
+  $scope.cartTip = false;
+
+  var showTip = function(){
+    $scope.cartTip = true;
+  };
+
+  var hideTip = function(){
+    $scope.cartTip = false;
+  };
 
   $scope.addItem = function() {
     $http.post('/', {items: $scope.inputModel})
       .success(function(data){
-        
+        if($scope.items === undefined){
+          showTip();
+          setInterval(hideTip, 3000);
+        }
         $scope.items = $scope.items || [];
-
         data.forEach(function(item){
           $scope.items.push(item);
         });
-        console.log($scope.items);
-
       });
   };
 
-  $scope.addToCart = function(element) {
+  $scope.addToCart = function($event) {
     $scope.cart = $scope.cart || [];
-    $scope.cart.push(element);
-    for(var i = 0; i < $scope.items.length; i++){
-      if($scope.items[i] === element){
-        $scope.items[i].splice(i, 1);
-      }
-    }
+    var item = angular.element($event.currentTarget);
+    var cart = angular.element(document.querySelector('#cart'));
+      console.log(cart.append);
+    cart.append(item.detach());
   };
 
 }]);
