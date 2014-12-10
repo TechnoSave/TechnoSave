6bc14ad062a3adfc91061ac82d12737be0ac65ee
Phil Elauria <phil.elauria@gmail.com>
2014-12-09 18:46:24 -0800

    fixed syntax error

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var js = require('../js/parserFile.js');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: "Techno$ave" });
+});
 
 router.post('/', function(req, res) {
   js.parser(req, res);
